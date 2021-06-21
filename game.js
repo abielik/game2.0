b2b09a2ab96415f290474c4ee5d2ae2794cf6498
Alan Bielik <abielik92@gmail.com>
2021-06-21 15:57:07 -0400

    deck and shuffle

---
+++ game.js
@@ -0,0 +1,52 @@
+const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+const values = [
+  'A',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+];
+
+function getDeck() {
+  const deck = new Array();
+  for (let suit of suits) {
+    for (let value of values) {
+      const card = {
+        value,
+        suit,
+      };
+      deck.push(card);
+    }
+  }
+  return deck;
+}
+
+function shuffle(deck) {
+  // switch the places of 2 random cards, 1000 times
+  for (let i = 0; i < 1000; i++) {
+    const randomCardIndex1 = Math.floor(Math.random() * deck.length);
+    const randomCardIndex2 = Math.floor(Math.random() * deck.length);
+    const tempCard = deck[randomCardIndex1];
+
+    deck[randomCardIndex1] = deck[randomCardIndex2];
+    deck[randomCardIndex2] = tempCard;
+  }
+}
+
+function determineWinner(playerOneCard, playerTwoCard) {
+  if (playerOneCard > playerTwoCard) {
+    return 'Player 1 wins this round';
+  } else if (playerOneCard < playerTwoCard) {
+    return 'Player 2 wins this round';
+  } else {
+    return 'Tie';
+  }
+}
